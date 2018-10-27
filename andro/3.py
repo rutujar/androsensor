a9896975340e6660ee4a6091dd79a19d7a927ba2
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-27 21:20:50 +0530

    Create 3.py

---
+++ andro/3.py
@@ -0,0 +1,6 @@
+andro_movelist = pd.read_csv("defaultdata.csv")  
+andro_movelist.head()  
+andro_movelist['LIGHT'].value_counts().plot(kind='bar')  
+plt.ylabel('Count')  
+plt.savefig("test.png")  
+plt.show()  
