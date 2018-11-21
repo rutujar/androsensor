73b119161d279b908578cc88d4282c4f1a1645a5
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-11-21 21:13:10 +0530

    Update and rename andro/4.py to 4.py

--- 4.py
+++ 4.py
@@ -2,6 +2,6 @@ from numpy import genfromtxt
 import pandas as pd  
 import numpy as np  
 import matplotlib.pyplot as plt  
-data = np.genfromtxt('defaultdata.csv', delimiter=',', names=['LIGHT', 'GRAVY'])  
+data = np.genfromtxt('data.csv', delimiter=',', names=['LIGHT', 'GRAVY'])  
 plt.plot(data['LIGHT'], data['GRAVY'])  
 plt.show() 
